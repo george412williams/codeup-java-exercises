b50555c458619d895a96f823e7f9f0ae886553bb
George Williams <george412williams@gmail.com>
2020-06-01 15:27:29 -0500

    ex done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,42 @@
 public class HelloWorld {
     public static void main(String[] args){
 //        System.out.println("Hello, World!");
-        System.out.print("Hello,");
-        System.out.print(" World!");
+        //System.out.print("Hello,");
+        //System.out.print(" World!");
+
+        int myFavoriteNumber = 8;
+        //String myString = 3.14159;
+        //char myStringVal = (char) myString;
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+        //long vars are integers and do not have floating decimals
+//        float myNumber = 3.14;
+//        double myNumberAllow = (double) myNumber;
+        double myNumber = 3.14;
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        //int class;
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+        //string
+        //two errors:
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        //14:lost data
+
+
     }
 }
 //ctrl r
