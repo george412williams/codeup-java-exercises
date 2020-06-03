bd907030a6d1448137d0ca9506c03387b5d57c49
George Williams <george412williams@gmail.com>
2020-06-03 11:07:31 -0500

    exercise 1a

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,10 +3,23 @@ import java.util.Arrays;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-    int i = 5;
+    int i = 2;
     while (i <= 15) {
         System.out.print(" " + i++);
     }
+
+//    do {
+//        System.out.println(i);
+//        int i = (int) i * (int) i;
+//        double square = (i * i);
+//        int isqr = (int) square;
+//        int i = isqr;
+//    } while (i < 1000000);
+
+//    for (boolean i < 1000000; i *= i) {
+//
+//    }
+
 
 
 
