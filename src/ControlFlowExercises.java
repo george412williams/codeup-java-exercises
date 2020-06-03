17dd6122abfdd6b67856ab273851156ec5c35839
George Williams <george412williams@gmail.com>
2020-06-03 11:30:41 -0500

    ex 1c

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,23 +3,27 @@ import java.util.Arrays;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-
-
-
+    long i = 2;
 
 //    while (i <= 15) {
 //        System.out.print(" " + i++);
 //    }
 
-    long i = 2;
-    do {
+    for (long i = 5; i <= 15; " " + i++) {
         System.out.println(i);
-        i *= i;
-    } while (i < 1000000);
+    }
 
-//    for (boolean i < 1000000; i *= i) {
-//
-//    }
+//    do {
+//        System.out.println(i);
+//        i *= i;
+//    } while (i < 1000000);
+
+    for (i; i < 1000000; i *= i) {
+        System.out.println(i);
+    }
+
+
+
 
 
 
