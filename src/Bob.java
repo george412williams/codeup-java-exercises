059f966a72df3f8f522d5c54e82d8ec71b6f6f5e
George Williams <george412williams@gmail.com>
2020-06-04 15:29:11 -0500

    first draft complete

---
+++ src/Bob.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String respBob = sc.next();
+
+        if (respBob.endsWith("?")) {
+            System.out.println("Sure.");
+        }else
+
+        if (respBob.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        }else
+
+        if (respBob.startsWith(" ")) {
+            System.out.println("Fine. Be that way!");
+        }else{
+            System.out.println("Whatever.");
+
+        }
+
+    }
+}
