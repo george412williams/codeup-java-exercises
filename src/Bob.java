0d795920dc7c8e3dc26d5e64d5ab4f8f876a1b61
George Williams <george412williams@gmail.com>
2020-06-04 15:59:50 -0500

    complete w refactor

--- src/Bob.java
+++ src/Bob.java
@@ -3,22 +3,15 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String respBob = sc.next();
-        System.out.println("This is Bob.");
+        String respBob = sc.nextLine();
         if (respBob.endsWith("?")) {
             System.out.println("Sure.");
-        }else
-
-        if (respBob.endsWith("!")) {
+        }else if (respBob.endsWith("!")) {
             System.out.println("Whoa, chill out!");
-        }else
-
-        if (respBob.startsWith(" ")) {
+        }else if (respBob.length() == 0) {
             System.out.println("Fine. Be that way!");
         }else{
             System.out.println("Whatever.");
-
         }
-
     }
 }
