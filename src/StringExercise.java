373d537879a1865801965c2dd750909d7440030b
George Williams <george412williams@gmail.com>
2020-06-04 15:07:54 -0500

    number 1 done

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String rock = "We don't need no education\nWe don't need no thought control";
+        System.out.println(rock);
+
+        String bumpy = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(bumpy);
+
+        String winDrive = "In windows, the main drive is usually C:\\";
+        System.out.println(winDrive);
+
+        String backslash = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(backslash);
+    }
+}
