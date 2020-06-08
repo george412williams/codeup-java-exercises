4634caef674b3ef292bb1e650e65a1572597d21f
George Williams <george412williams@gmail.com>
2020-06-08 16:28:49 -0500

    working through ex 5

---
+++ src/HighLow.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class HighLow {
+
+
+//    public static long gameDev(String game) {
+//        Scanner sc = new Scanner(System.in);
+//        Random rand = new Random();
+//        System.out.println("Enter number guess (1-100): ");
+//        long guess = sc.nextInt();
+//        long randHiLo = rand.nextInt(100);
+//    }
+
+
+    public static void main(String[] args) {
+        //gameDev();
+    }
+}
