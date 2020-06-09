49d1db9110cad32ca5f585c5e2dfd0103f4c933a
George Williams <george412williams@gmail.com>
2020-06-09 12:03:56 -0500

    ex compl check string

---
+++ src/DishTest.java
@@ -0,0 +1,12 @@
+public class DishTest {
+
+
+    public static void main(String[] args) {
+
+        Dish dish1 = new Dish();
+        Integer costInCents = 800;
+        String nameOfDish = "Sammich";
+        boolean wouldRecommend = true;
+        dish1.printSummary();
+    }
+}
