e762a15797d38836f2ddb002c8d72e0a4d698112
George Williams <george412williams@gmail.com>
2020-06-09 16:23:15 -0500

    making getsets

--- src/Dish.java
+++ src/Dish.java
@@ -7,9 +7,15 @@ public class Dish {
         System.out.printf("Cost: %d%n" + "Name: %s%n" + "Recommended: %b%n", costInCents, nameOfDish, wouldRecommend);
 
     }
-    public void setCostInCents(int costInCents) {
-
+    public int getCostInCents(int costInCents) {
+        return costInCents;
     }
+
+    public void setCostInCents(int costInCents) {
+        this.costInCents = costInCents;
+    }
+
+
 
 
 }
