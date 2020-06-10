4608e5ad91f8cc06223e1aded48c680398e51254
George Williams <george412williams@gmail.com>
2020-06-10 11:24:01 -0500

    almost there w 3

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,12 +4,24 @@ import java.util.Scanner;
 
 public class Input {
 
-    static Scanner input = new Scanner(System.in);
-    private String scanner;
+    static Scanner scanner;
+    public Scanner getScanner(){
+        return scanner;
+    };
 
     public Input (String thing) {
-
+        this.scanner = new Scanner(System.in);
+        //this creates a new one every time its used, aka called
     }
+
+    public void getString(){
+        System.out.println("Please type in a string.");
+        String userBoolean = scanner.nextLine();
+        System.out.println();
+    }
+
+    //public void ... need to validate users input back atem
+
 
 
 
