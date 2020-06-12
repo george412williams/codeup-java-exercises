d837e664cc96a20aff627c7ae0736f5a757e2821
George Williams <george412williams@gmail.com>
2020-06-12 16:00:31 -0500

    item 1 done

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,34 +2,22 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-    public static Person[] addPerson(Person[] people){
-        Person[] newPerson = Arrays.copyOf(people, people.length + 1);
-        return newPerson;
+    public static Person[] addPerson(Person[] people, Person newPerson) {
+        Person[] newPeople = Arrays.copyOf(people, people.length + 1);//4th spot made
+        newPeople[newPeople.length - 1] = newPerson;//newPeople.length is now 4 that does not exist so -1
+        //System.out.println(Arrays.toString(newPeople[newPeople.length]);
+        return newPeople;
     }
-
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
-//        Person[] exPeople = {new Person(String), new Person("Curly"), new Person("Shep")};
         Person[] people = new Person[3];
-//        //expandGroup[0] = new Person();
         people[0] = new Person("Mo");
         people[1] = new Person("Curly");
         people[2] = new Person("Shep");
-//        for (Person expandGroup: current){
-//            System.out.println(expandGroup.getName());
-        //}
-        for (Person current: people){
-            System.out.println(current.getName());
-        }
-        Person[] newPerson = Arrays.copyOf(people,people.length + 1);
-        //System.out.println(Arrays.toString(newPerson));
-        newPerson[3] = new Person("Mike");
-        //System.out.println(addPerson(Arrays.toString());
-        //System.out.println(people);
-        System.out.println(Arrays.toString(addPerson(people)));
-
-
+        people = addPerson(people, new Person("Larry"));
+        for (Person name: people){
+            System.out.println(name.getName());
         }
     }
-
+}
