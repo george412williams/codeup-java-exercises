f8ec973067833f74cda399e3f29910d9261ebf0d
George Williams <george412williams@gmail.com>
2020-06-12 12:31:48 -0500

    lunchtime save

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -9,12 +9,16 @@ public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
-        Person[] expandGroup = new Person[3];
-        expandGroup[0].setName("Mo");
-        expandGroup[1].setName("Curly");
-        expandGroup[2].setName("Shep");
-        for (Person currentGroup: expandGroup){
-            System.out.println(currentGroup);
+//        Person[] expandGroup = new Person[3];
+//        //expandGroup[0] = new Person();
+//
+//        expandGroup[0].setName("Mo");
+//        expandGroup[1].setName("Curly");
+//        expandGroup[2].setName("Shep");
+//        for (Person person: expandGroup){
+//            System.out.println(person.getName());
+        String[] group = {"Mo", "Curly", "Shep"};
+
         }
     }
-}
+
