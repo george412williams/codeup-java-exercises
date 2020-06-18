5b0cb643d0161d649fd29718cc258c0a0e6c13f9
George Williams <george412williams@gmail.com>
2020-06-18 09:14:59 -0500

    grocery bonus work

---
+++ src/groceryBonus/GroceryItem.java
@@ -0,0 +1,20 @@
+package groceryBonus;
+
+import java.util.Arrays;
+
+//scanner
+
+public class GroceryItem {
+//boolean to aske do you want to make a groc list
+//when init the array, = new GroceryItem[1];
+    //with empty constructor in here and make
+    //for loop in test class as in examples
+
+    //groceryItemsArray = new GroceryItem[arrayLength];
+        //does not fully work in order to add multiple items
+
+    public static void main(String[] args) {
+
+    }
+
+}
