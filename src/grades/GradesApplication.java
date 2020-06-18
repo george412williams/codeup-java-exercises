8aa549e31f992e0647ec75d0879edac6481d69b5
George Williams <george412williams@gmail.com>
2020-06-18 16:57:57 -0500

    eod save ex ongoing

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,18 @@
+package grades;
+import java.util.HashMap;
+import util.Input;
+
+public class GradesApplication {
+
+    static Input input = new Input();
+
+    public static void main(String[] args) {
+
+        HashMap<String,Student> students = new HashMap<>();
+        //students.put("gitHubCowboy", );
+        Student s1  = new Student("nameie", 1);
+        //do if else while
+        //public static void searchUser...
+
+    }
+}
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,22 +1,83 @@
 package grades;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
 
+
+
+
 public class Student {
+
+    private Scanner scanner;
+    public Scanner getScanner(){
+        return scanner;
+    };
+
+
+    //need public Student class!!!!!!!!
+
+    this.grades = ArrayList<Integer>
 
     private String name;
     private int grade;
+
+    List<Integer> grades = new ArrayList<Integer>();
+
+    //instru used StringBuilder class
 
     public String getName() {
         return name;
     }
 
-    List<Integer> grades = new ArrayList<>();
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //does the student here take in both name and grade
+    public Student(String name, int grade){
+
+    }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        for (int grade : grades) {
+            int sum = 0;
+            //(grade += grade)/(grades.size());
+            sum += grade;
+
+        }
+
+
+        return getGradeAverage();
+    }
+
+    public String GradesApplication(){
+
+    }
+
+
+
 
 
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Options: ...: ");
+        scanner.nextInt();
+        //in.getInt();
+
+
+        //options interface:
+        //1. Print Student with Grade
+        //2. Add a Student Grade
+
+        //getName
 
     }
 }
