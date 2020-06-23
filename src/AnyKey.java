a8a90756a445c30020bae5b5981227aaf8f067b3
George Williams <george412williams@gmail.com>
2020-06-23 15:52:37 -0500

    bring in other resources and new concept

--- src/AnyKey.java
+++ src/AnyKey.java
@@ -1,5 +1,8 @@
 import util.Input;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
+
 public class AnyKey {
 
 
@@ -48,6 +51,28 @@ public class AnyKey {
     }
 
 
+//I've also found it good practice to empty the input stream prior to issuing the prompt. Something like:
+//
+//public void prompt(String s) throws IOException {
+//    while ((int count = System.in.available()) > 0) {
+//        System.in.skip(count);
+//    }
+//    if (s != null) {
+//        System.out.print(s);
+//    }
+//
+//    System.in.read();
 
+//}
+//
+//Of course, this code, and the examples that precede it in this thread,
+// don't return on _any_ character. Technically, they only return when a line termination sequence is encountered on the input stream.
+// This is because the default 'console mode' for Java is what POSIX calls line-mode.
+// To make 'press any key' work, you would have to be able to set the stream to character-mode so that
+// each character received would cause an interrupt of the read. Perhaps the JDK 1.4 nio capabilities make this possible,
+// but AFAIK there is no way to set System.in so that it receives each character as it is typed.
+//
+//Best regards,
+//Jim Cakalic
     }
 
